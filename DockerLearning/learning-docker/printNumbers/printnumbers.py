78178979d7ab5368c9de7a23a393436e5fc0a153
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-09-22 16:17:41 -0500

    Exploring docker containers

---
+++ DockerLearning/learning-docker/printNumbers/printnumbers.py
@@ -0,0 +1,11 @@
+#code to print numbers from 1 to 100
+
+# create a function
+def printNum():
+    print("Numbers:")
+    for num in range(1,101):
+        print(num)
+
+
+#calling the function
+printNum()
